a77e825164c960f7bd7221332cc9a18bb8312d10
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-16 15:26:19 -0500

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main: program beginning
+ * Return: always zero
+ * /
 int main(void)
 {
 
 puts("\"Programming is like building a multilingular puzzle\n");
-return 0;
+return (0);
 }

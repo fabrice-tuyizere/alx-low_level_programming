1100f1ba8f6ce72acef6e0127482b14bbf96618e
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-22 11:08:38 -0500

    adding another 4 file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - it will excute program
+ * @c: parameter to be passed
+ *
+ * Return: return 1 if parameter passed is alphabetic character otherwise zero
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') ||(c >= 'A' && c <= 'Z'))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}

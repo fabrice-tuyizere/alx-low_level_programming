58d802d22e5fa7ca5cd6e47ae01002e5402660bd
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-22 12:09:24 -0500

    adding another 6 file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - generate absolute value of number
+ *
+ * Return: return value
+ */
+int _abs(int n)
+{
+int cal; 
+if (n <= 0)
+{
+	cal=-n;
+	return (cal);
+}
+else
+	return(n);
+
+}

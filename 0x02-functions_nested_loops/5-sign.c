6ffbdbad8434e8cfb5d4e04519b0b22eb74cfea7
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-22 11:40:44 -0500

    adding another 5 file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - execute the output
+ * @n: a parameter to pass
+ *
+ * Return: return signs and number acording to the value of n
+ */
+int print_sign(int n)
+{
+if (n > 0 )
+{
+	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
+else
+{
+	_putchar('-');
+	return (-1);
+}
+}

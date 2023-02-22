96413e559606b724b26259c852534b341736191e
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-22 07:20:43 -0500

    new commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,17 @@
 /**
  * print_alphabet_x10 - print output
  */
- void print_alphabet_x10(void)
- {
- char i;
- int count;
+void print_alphabet_x10(void)
+{
+char i;
+int count;
 
- for (count = 0; count < 10; count ++)
- {
- for(i = 'a'; i <= 'z'; i++)
- {
- _putchar(i);
- }
- _putchar('\n');
- }
- }
+for (count = 0; count < 10; count ++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+	_putchar('\n');
+}
+}

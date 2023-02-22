ee958110089c1468a7ba8d51c4d51935a3a48f76
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-22 10:36:40 -0500

    new commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower - produce an out put 
- * Return: return one if success other wise 0
+*_islower - produce an out put 
+* Return: return one if success other wise 0
  */
 int _islower(int c)
 {
-if(c>='a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 	return (1);
 }

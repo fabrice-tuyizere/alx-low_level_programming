d30ab1d0657107ff58d7fc33d7f329d621e3c860
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-23 13:48:32 -0500

    adding changes to file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print out our numbers
+ * Return: return 0
+ */
+void print_numbers(void)
+{
+int i;
+
+for(i = '0'; i <= '9'; i++)
+{
+	_putchar(i);
+}
+}

04b5c17c24df5fb3d1aaa648260ebe33964abbe2
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-23 13:08:11 -0500

    adding another 3 files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * mul - perform calculations
+ * @a: 1st parameter
+ * @b: second parameter
+ *
+ * Return: return output
+ */
+int mul(int a, int b)
+{
+int result;
+
+result= a*b;
+	return(result);
+}

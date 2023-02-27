64e5c9f66ac1a4e20f885f1e19d2008405c52e7c
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-27 05:56:35 -0500

    pushing  changes to files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function that will swap two numbers
+ *
+ * @a: first number to be passed
+ * @b: second number to be passed
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}

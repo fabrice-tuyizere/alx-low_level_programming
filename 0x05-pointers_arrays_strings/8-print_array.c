58a802a0d5bea59335d64f6e7a84fe51f1e930e4
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-27 15:14:14 -0500

    adding changes to file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * prints_array - print the output to the standard output
+ *
+ * @a: parameter
+ * @n:size of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++)
+	{
+		printf("%d",a[i]);
+		if(i == n-1)
+			continue;
+		printf(", ");
+	}
+	printf("\n");
+}

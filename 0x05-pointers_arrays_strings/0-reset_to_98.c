56a4ff3ba108b73b8acd0a3594bafd31d7b97541
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-27 05:44:03 -0500

    pushing  changes to files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 
 #include "main.h"
 /**
- * reset_to_98 - change parameter to the 98
- * @n: parameter
+ * reset_to_98 - Change parameter to the 98
+ *
+ * @n: Parameter to be passed
  */
+
 void reset_to_98(int *n)
 {
-	*n=98; 
+	*n = 98;
 }

702ba1120e0520b13ba8d6baf4fae4baa6af0408
fabrice-tuyizere <tuyiface@gmail.com>
2023-02-27 15:52:52 -0500

    adding changes to file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * _strcpy - function to print copy of another pointer 
+ * _strcpy - function to print copy of another pointer
  * @src: source parameter
  * @dest: destination parameter
+ *
+ * Return: return the new string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i=0;
+	int i = 0;
 
 	while (src[i])
 	{

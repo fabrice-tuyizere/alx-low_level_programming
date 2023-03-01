e46fa7ade0db16472a362ae8e194849b5834b475
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-01 15:50:50 -0500

    adding changes to file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,10 @@
  * @s1: parameter to pass string 1
  * @s2: string 2 to b passed
  *
- * Return -return the value accordingly
+ * Return: -return the value accordingly
  */
 int _strcmp(char *s1, char *s2)
-{ 
+{
 	while ((*s1 &&  *s2) && (*s1 == *s2))
 	{
 		s1++;

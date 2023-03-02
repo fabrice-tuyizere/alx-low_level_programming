8cb67653260d076fbc9cd4da6fbe47d5c4180f20
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-02 09:32:33 -0500

    adding changes to file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,19 @@
 #include "main.h"
+/**
+ * *cap_string - function definition
+ * @s: parameter string to be passed
+ *
+ * Return: return output string
+ */
 char *cap_string(char *s)
 {
 	int i=0;
+
 	while(s[i++])
 	{
-		if(i== 0 && (s[i] >= 'a' && s[i]<='z'))
-			s[i]='A'+s[i]-'a';
-		if ((s[i]== '\t' ||
+		if(i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = 'A' + s[i] - 'a';
+		if ((s[i] == '\t' ||
 			s[i] == '\n' ||
 			s[i] == ',' ||
 			s[i] == ';' ||
@@ -18,8 +25,8 @@ char *cap_string(char *s)
 			s[i] == '{' ||
 			s[i] == '}' ||
 			s[i] == ' ' ||
-			s[i] == '.') && (s[i + 1] >='a' && s[i + 1]<='z'))
-				s[i+1]='A'+s[i+1]-'a';
+			s[i] == '.') && (s[i + 1] >='a' && s[i + 1] <= 'z'))
+				s[i + 1] = 'A' + s[i + 1] - 'a';
 	}
 	return (s);
 }

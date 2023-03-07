a5f4e88f7219979a07ff958ada19cd4f8fb60ce2
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-07 15:10:56 -0500

    adding changes to file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - parameter definition
+ * @s: pointer to pointer parameter
+ * @to: parameter character
+ */
+void set_string(char **s, char *to)
+{
+	*s = to
+}

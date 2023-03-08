909db093a28a325e6c7db0a4764241cb7d0bb0a2
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-08 13:28:12 -0500

    adding changes to file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,7 @@
 void _puts_recursion(char *s);int _putchar(char c);
+    void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _sqrt_recursion(int n);

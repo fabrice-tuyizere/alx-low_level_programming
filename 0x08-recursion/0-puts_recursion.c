0cd171ed0b794ce824eacbbf1ae7ce500d665fd1
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-08 13:03:42 -0500

    adding changes to file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,18 @@
 #include "main.h"
+/**
+ * _puts_recursion - entry function
+ * @s: paramater to be passed
+ */
 void _puts_recursion(char *s)
 {
-	if(*s=='\0')
+	if (*s == '\0')
+	{
 		return;
-	_putchar(*s);
-	_puts_recursion(s+1);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
 }

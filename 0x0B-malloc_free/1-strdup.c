c8864c21daf17ee5b6dccf314d5b2cd00643cf2b
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-15 12:20:54 -0400

    adding changes to file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function definition to duplicate
+ * @str: parameter string
+ * Return: return the pointer to the new string;
+ */
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	for ( ; str[size] != '\0'; size++)
+		;
+	ptr = (char *)malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		ptr[i] = str[i];
+	return (ptr);
+	free(ptr);
+}

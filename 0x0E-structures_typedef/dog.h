d15c20d70463c35cace9b63a1d895d71df70d29f
fabrice-tuyizere <tuyiface@gmail.com>
2023-03-20 16:45:47 -0400

    adding changes to file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structure declaration
+ * @name: meber type of string
+ * @owner: membertype of string
+ * @age: membertype of float
+ * Description: defining the dog
+ */
 struct dog
 {
 	char *name;
